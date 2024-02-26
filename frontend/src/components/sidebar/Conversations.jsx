77d54a417a02a837d380fda811484c7655d6b016
Vijay Digambar Kusekar <vijay.kusekar2003@gmail.com>
2024-02-26 19:48:17 +0530

    2nd part completed

---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,34 @@
+import Conversation from "./Conversation";
+
+const Conversations = () => {
+	return (
+		<div className='py-2 flex flex-col overflow-auto'>
+			<Conversation />
+			<Conversation />
+			<Conversation />
+			<Conversation />
+			<Conversation />
+			<Conversation />
+		</div>
+	);
+};
+export default Conversations;
+
+
+
+// STARTER CODE SNIPPET
+// import Conversation from "./Conversation";
+
+// const Conversations = () => {
+// 	return (
+// 		<div className='py-2 flex flex-col overflow-auto'>
+// 			<Conversation />
+// 			<Conversation />
+// 			<Conversation />
+// 			<Conversation />
+// 			<Conversation />
+// 			<Conversation />
+// 		</div>
+// 	);
+// };
+// export default Conversations;
---
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -0,0 +1,24 @@
+import { BiLogOut } from "react-icons/bi";
+
+const LogoutButton = () => {
+  return (
+    <div className='mt-auto'>
+        <BiLogOut className="w-6 h-6 text-white cursor-pointer"/>
+    </div>
+  );
+}
+
+export default LogoutButton;
+
+//Starter code snippet
+// import { BiLogOut } from "react-icons/bi";
+
+// const LogoutButton = () => {
+//   return (
+//     <div className='mt-auto'>
+//         <BiLogOut className="w-6 h-6 text-white cursor-pointer"/>
+//     </div>
+//   );
+// }
+
+// export default LogoutButton;
